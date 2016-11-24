3c13d4618bab5db9fd51f8eb21669cf7a1ce71f4
Tushar Baraiya <tusharpr@buffalo.edu>
2016-11-24 07:14:01 -0500

    working device and platform

--- device.c
+++ device.c
@@ -10,30 +10,38 @@
 
 
 int main(){
-int port = 9887;
 char msg[9];
-char* timberlake_ip = "128.205.36.8";
-char* my_ip = "128.205.163.14";
-struct sockaddr_in udp;
-int send_udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
-memset(&udp,0,sizeof(udp));
-udp.sin_family=AF_INET;
-inet_aton(my_ip, &udp.sin_addr);
-udp.sin_port=htons(port);
-
 char id[3];
 char device_name[20];
 char s1[2];
 char s2[2];
 char s3[2];
+char* timberlake_ip = "128.205.36.8";
+char* my_ip = "71.186.225.249";
+char* local_addr="127.0.0.1";
+int port = 9777;
 
+struct sockaddr_in udp;
+int send_udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
+if(send_udp_fd == -1){
+printf("sockfd failed\n");
+}
+
+memset(&udp,0,sizeof(udp));
+udp.sin_family=AF_INET;
+inet_aton(local_addr, &udp.sin_addr);
+udp.sin_port=htons(port);
+/*
+char* ms = "12345";
+int num= sendto(send_udp_fd, ms, 5, 0, (struct sockaddr *)&udp, sizeof(udp));
+printf("sent %d \n", num);
+*/
 printf("Create an IOT Device...\n");
 printf("Enter Device ID: ");
 scanf("%s",id);
 printf("Enter Device Name: ");
 scanf("%s",device_name);
 printf("Successfuly created devide, now you may start sending sensor data to the platform... ");
-
 while(1){
 	printf("\nEnter sensor_1 value : ");
 	scanf("%s",s1);
@@ -45,8 +53,9 @@ while(1){
 	strcat(msg,s1);
 	strcat(msg,s2);
 	strcat(msg,s3);	
-	printf("msg : %s \n",msg);	
-	//sendto(send_udp_fd, msg, 5, 0, (struct sockaddr *)&udp, sizeof(udp));
+	sendto(send_udp_fd, msg, 9, 0, (struct sockaddr *)&udp, sizeof(udp));
 	printf("Succsessfuly sent the data to the platform");
 }
+
+return 0;
 }
--- platform.c
+++ platform.c
@@ -10,9 +10,14 @@
 
 
 int main(){
-int port = 9887;
+int port = 9777;
 printf("Creating UDP socket... ");
 int udp_rcvfd=socket(AF_INET, SOCK_DGRAM, 0);
+if(udp_rcvfd == -1){
+printf("sockfd failed\n");
+return 0;
+}
+
 struct sockaddr_in udp_rcv;
 memset(&udp_rcv,0,sizeof(udp_rcv));
 udp_rcv.sin_family=AF_INET;
@@ -20,14 +25,21 @@ udp_rcv.sin_addr.s_addr=htonl(INADDR_ANY);
 udp_rcv.sin_port=htons(port);
 printf(": Successful\n");
 printf("Binding ... ");
-bind(udp_rcvfd, (struct sockaddr *)&udp_rcv, sizeof(udp_rcv));
+int bnd = bind(udp_rcvfd, (struct sockaddr *)&udp_rcv, sizeof(udp_rcv));
+if(bnd == -1){
+printf("bind failed\n");
+return 0;
+}
 printf(": Successful\n");
 printf("Listening on PORT : %d\n", port);
+
+
 while(1){
 	uint8_t rcvfrm[200];
 	int slen = sizeof(udp_rcv);
-	recvfrom(udp_rcvfd, rcvfrm, 200, 0, (struct sockaddr *)&udp_rcv, &slen);
+	recvfrom(udp_rcvfd, rcvfrm, 199, 0, (struct sockaddr *)&udp_rcv, &slen);
 	printf("recieved %s\n ",rcvfrm);
 	}
+
 return 0;
 }
