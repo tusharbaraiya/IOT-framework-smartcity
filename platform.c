9b8b2fdb3f4fdef33fd9ccfe94f7cd4ae166cab2
Tushar Baraiya <tusharpr@buffalo.edu>
2016-11-24 20:28:13 -0500

    added database creation

--- platform.c
+++ platform.c
@@ -7,10 +7,29 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <my_global.h>
+#include <mysql.h>
 
+int create_database(char* );
 
 int main(){
 int port = 9777;
+char db_name[20];
+printf("Configuring Platform...\n");
+printf("Creating Database...\n");
+printf("Enter Database name: ");
+scanf("%s",db_name);
+int db_op = create_database(db_name);
+printf("Database creation :");
+
+if(db_op==-1){
+printf("Failed, check logs\n");
+return -1;
+}
+
+printf("Successful\n");
+
+
 printf("Creating UDP socket... ");
 int udp_rcvfd=socket(AF_INET, SOCK_DGRAM, 0);
 if(udp_rcvfd == -1){
@@ -38,8 +57,37 @@ while(1){
 	uint8_t rcvfrm[200];
 	int slen = sizeof(udp_rcv);
 	recvfrom(udp_rcvfd, rcvfrm, 199, 0, (struct sockaddr *)&udp_rcv, &slen);
-	printf("recieved %s\n ",rcvfrm);
+	printf("recieved %s\n",rcvfrm);
 	}
 
 return 0;
+}
+
+int create_database(char* name){
+MYSQL *con = mysql_init(NULL);
+  if (con == NULL) 
+  {
+      fprintf(stderr, "%s\n", mysql_error(con));
+      return -1;
+  }
+  if (mysql_real_connect(con, "localhost", "root", "root_pswd", 
+          NULL, 0, NULL, 0) == NULL) 
+  {
+      fprintf(stderr, "%s\n", mysql_error(con));
+      mysql_close(con);
+      return -1;
+  }  
+char* qry = "CREATE DATABASE ";
+char full_qry[30];
+
+strcpy(full_qry,qry);
+strcat(full_qry,name);
+ 
+  if (mysql_query(con, full_qry)) 
+  {
+      fprintf(stderr, "%s\n", mysql_error(con));
+      mysql_close(con);
+      return -1;
+  }
+return 1;
 }
